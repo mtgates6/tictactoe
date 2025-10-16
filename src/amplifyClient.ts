c3c9ea9757b2cbce42a7c620888bed95453f88a7
mtgates6 <56485852+mtgates6@users.noreply.github.com>
2025-10-15 18:48:13 -0700

    add local/prod envs

--- src/amplifyClient.ts
+++ src/amplifyClient.ts
@@ -2,13 +2,29 @@
 
 import type { Schema } from '../amplify/data/resource';
 import { Amplify } from 'aws-amplify';
-// import { generateClient, Client} from '@aws-amplify/datastore';
-import { generateClient } from 'aws-amplify/data';
+import { generateClient, type Client } from 'aws-amplify/data';
 
-Amplify.configure({
-    // Add your Amplify configuration here
-    aws_project_region: 'us-west-1',
-});
+let client: Client<Schema>;
 
+if (import.meta.env.VITE_USE_LOCAL_AMPLIFY === 'true') {
+  const initClient = async () => {
+    const amplifyModule = await import('../amplify_outputs.json');
+    const amplifyOutputs = amplifyModule.default;
+    Amplify.configure(amplifyOutputs);
+  };
+  void initClient();
+} else {
+    // Configure Amplify minimally so generateClient can work in deployed environment
+    Amplify.configure({
+        API: {
+            GraphQL: {
+            endpoint: process.env.VITE_GRAPHQL_ENDPOINT ?? '',
+            defaultAuthMode: 'apiKey', 
+            },
+        },
+    });
+}
+
+client = generateClient<Schema>();
 // Then generate the client
-export const client = generateClient<Schema>();
+export {client}; 
