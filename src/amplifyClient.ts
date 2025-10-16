8fafdeb9d227c73ed8a2c614f33d31d087913320
mtgates6 <56485852+mtgates6@users.noreply.github.com>
2025-10-15 18:19:27 -0700

    add region

--- src/amplifyClient.ts
+++ src/amplifyClient.ts
@@ -1,10 +1,14 @@
 
 
-//import { a } from '@aws-amplify/backend';
 import type { Schema } from '../amplify/data/resource';
-//import { Amplify } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 // import { generateClient, Client} from '@aws-amplify/datastore';
 import { generateClient } from 'aws-amplify/data';
+
+Amplify.configure({
+    // Add your Amplify configuration here
+    aws_project_region: 'us-west-1',
+});
 
 // Then generate the client
 export const client = generateClient<Schema>();
